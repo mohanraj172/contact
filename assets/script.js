996138bfff82388d5035c0890eda52c19192ffdf
Dhileepguru <dhileepguru497@gmail.com>
2024-05-12 09:17:41 +0530

    updated script to displayData

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,12 @@
 fetch('https://jsonplaceholder.typicode.com/posts/1')
 .then(response => response.json())
 .then(data => {
-    console.log(data)
+    localStorage.setItem('fetchedData', JSON.stringify(data));
+    displayData(data);
+   
 })
+.catch(error => console.error('Error fetching data:', error));
+
+function displayData(data) {
+    console.log(data);
+}
